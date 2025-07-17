0ff1d051b07f6f7f88de941f2beac39133ea757e
mostafa.gaafar <mostafa.gaafar@sqrm.net>
2025-07-18 01:58:42 +0300

    sign in page

--- src/app/core/components/auth/auth.component.ts
+++ src/app/core/components/auth/auth.component.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { TabsModule } from 'primeng/tabs';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+import { SignInComponent } from '../sign-in/sign-in.component';
 @Component({
   selector: 'auth',
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
-  standalone: true
+  standalone: true,
+  imports: [CommonModule, TabsModule, CardModule, SignInComponent, SignUpComponent]
 })
 export class AuthComponent {
   
--- src/app/core/components/sign-in/sign-in.component.ts
+++ src/app/core/components/sign-in/sign-in.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { Fluid } from 'primeng/fluid';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
-  standalone: true
+  standalone: true,
+  imports: [CardModule, ButtonModule, Fluid, FormsModule, InputTextModule]
 })
 export class SignInComponent {
   
