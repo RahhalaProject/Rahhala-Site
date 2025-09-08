958afeb41833f97f9ebfdc7db8b7295aa251de40
mostafa.gaafar <mostafa.gaafar@sqrm.net>
2025-09-09 00:21:34 +0300

    profile icon menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,18 +1,17 @@
 import { Component, Input } from '@angular/core';
-import { MegaMenuItem } from 'primeng/api';
+import { MegaMenuItem, MessageService } from 'primeng/api';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { CommonModule } from '@angular/common';
 import { MegaMenu } from 'primeng/megamenu';
 import { RouterModule } from '@angular/router';
-import { Select } from 'primeng/select';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { PrimeNG } from 'primeng/config';
 import { FormsModule } from '@angular/forms';
 import { DOCUMENT } from '@angular/common';
 import { Inject } from '@angular/core';
-
+import { PopoverModule } from 'primeng/popover';
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
@@ -26,9 +25,9 @@ import { Inject } from '@angular/core';
     ButtonModule,
     CommonModule,
     RouterModule,
-    Select,
     FormsModule,
     TranslateModule,
+    PopoverModule,
   ],
   providers: [TranslateService],
 })
@@ -37,7 +36,7 @@ export class HeaderComponent {
   items: MegaMenuItem[] | undefined;
   supportLanguages = ['en', 'ar'];
   selectedLanguage!: string;
-
+  items2: any;
   constructor(
     readonly config: PrimeNG,
     readonly translateService: TranslateService,
@@ -82,6 +81,32 @@ export class HeaderComponent {
     // });
 
     const selectedLang = lang.value || lang; // handle both { value: 'ar' } or 'ar'
+
+    this.translateService.use(selectedLang);
+    localStorage.setItem('lang', selectedLang);
+
+    this.translateService.get('primeng').subscribe((res) => {
+      this.config.setTranslation(res);
+    });
+
+    // Change direction dynamically
+    const html = this.document.documentElement as HTMLElement;
+    html.setAttribute('dir', selectedLang === 'ar' ? 'rtl' : 'ltr');
+
+    // Optionally: Change a custom class (for styling)
+    html.classList.remove('rtl', 'ltr');
+    html.classList.add(selectedLang === 'ar' ? 'rtl' : 'ltr');
+
+    // Optional hard reload (only if needed):
+    // location.reload(); // not ideal; use only if component doesn't detect dir change
+  }
+
+  changeLang() {
+    const currentLang =
+      this.translateService.currentLang || localStorage.getItem('lang') || 'en';
+
+    // عكس اللغة (اختياري لو عايز toggle)
+    const selectedLang = currentLang === 'ar' ? 'en' : 'ar';
 
     this.translateService.use(selectedLang);
     localStorage.setItem('lang', selectedLang);
