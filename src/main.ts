d3c855dd01e5e9fc8d4ffcbe6a4b0e4f5afc9b8f
mostafa.gaafar <mostafa.gaafar@sqrm.net>
2026-04-24 14:22:07 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeAr from '@angular/common/locales/ar';
 import { config } from './app/app.config';
 import { App } from './app/app';
+
+registerLocaleData(localeAr);
+registerLocaleData(localeAr, 'ar-SA');
 
 bootstrapApplication(App, config).catch((err) => console.error(err));
